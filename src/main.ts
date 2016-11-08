29defcb310e171833833f5313fde639eaf137ef4
Isabelle <isa.bidou@gmail.com>
2016-11-08 11:13:24 +0000

    move files to src folder

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
+
+
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app.module';
+import { AppModule } from './app/app.module';
 
 //http://stackoverflow.com/a/39520087/1731667
 //https://github.com/gulpjs/gulp/blob/master/docs/getting-started.md
